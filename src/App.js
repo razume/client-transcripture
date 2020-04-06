6d17e5f43b2ad43add1aa55e50a9cfd549a54c92
tannerbyers <programtanner@gmail.com>
2020-04-05 21:03:55 -0400

    New base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
-// hi
+import axios from "axios";
+
+
+const clientID = "wvaVD6itTme4P9YBmPMZkg"
+
+// Set Dev variables and Prod Variables 
+
+if (process.env.NODE_ENV === "development") {
+  var redirectURL = "http://localhost:3000/"
+} else {
+  var redirectURL = "https://client-transcipture.herokuapp.com/"
+}
+
+console.log("redirectURL", redirectURL)
+const url = ('https://zoom.us/oauth/authorize?response_type=code&client_id=' + clientID + '&redirect_uri=' + redirectURL)
 function App() {
+
+  useEffect(() => {
+    if (window.location.href.indexOf("code") > -1) {
+      console.log(window.location.href)
+      let currenturl = window.location.href
+      let StartPositionCode = window.location.href.indexOf("code") + 5
+      let lastindexofurl = window.location.href.length
+      let code = (currenturl.slice(StartPositionCode, lastindexofurl))
+      console.log("Code:", code)
+
+
+      axios.post('http://localhost:5000/api/auth', {
+        code: code
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <a href={url}>
+        <button>Click Here</button>
+      </a>
     </div>
   );
 }
