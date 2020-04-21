49d112223b4cfe6acb717f9886ccdc7e29e03704
Tristan <tristancc99@gmail.com>
2020-04-21 15:18:54 -0400

    moved the requests back into transcripts because of auth issue

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,7 @@
-
-import React, { useState, useEffect } from "react";
-import MainPage from "./MainPage";
-import { Button, Paper } from "@material-ui/core";
-import theme from "../styled-components/theme";
+import React, { useState, useEffect } from "react"
+import MainPage from "./MainPage"
+import { Button, Paper } from "@material-ui/core"
+import theme from "../styled-components/theme"
 import {
   Box,
   Heading,
@@ -16,53 +15,7 @@ import axios from "axios"
 import "../App.css"
 
 function Dashboard({ setAuthCode, posturl, redirectURL }) {
-
   const [location, setLocation] = useState("")
-  let [transcripts, setTranscripts] = useState([])
-  let [folders, setFolders] = useState()
-
-  const getFolders = () => {
-    axios
-      .get(posturl + "/api/db/folders")
-      .then((fold) => setFolders(fold.data.folders))
-  }
-
-  const requestMeetings = () => {
-    axios
-      .get(posturl + "/api/recordings")
-      .then(function (response) {
-        // handle success
-        let videoLocation = document.querySelector("#PutSampleVideo")
-        console.log(videoLocation)
-        var video = document.createElement("video")
-        var transcript = document.createElement("p")
-        video.src = response.data.filePath
-        video.controls = true
-        video.style.width = "200px"
-        transcript.innerHTML = response.data.transcription
-        videoLocation.append(transcript)
-        videoLocation.append(video)
-        console.log(response.data)
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error)
-      })
-      .then(function () {
-        axios
-          .get(posturl + "/api/db/transcripts")
-          .then((res) => setTranscripts(res.data))
-      })
-
-    axios
-      .get(posturl + "/api/db/transcripts")
-      .then((res) => setTranscripts(res.data))
-  }
-
-  useEffect(() => {
-    requestMeetings()
-    getFolders()
-  }, [])
 
   const LogOutClicked = () => {
     localStorage.removeItem("code")
@@ -86,17 +39,7 @@ function Dashboard({ setAuthCode, posturl, redirectURL }) {
         />
       )
     } else if (location === "Transcripts") {
-
-      return (
-        <Transcripts
-          posturl={posturl}
-          setLocation={setLocation}
-          transcripts={transcripts}
-          setTranscripts={setTranscripts}
-          setFolders={setFolders}
-          folders={folders}
-        />
-      )
+      return <Transcripts posturl={posturl} setLocation={setLocation} />
     } else if (location === "Reports") {
       return <Reports setLocation={setLocation} />
     }
