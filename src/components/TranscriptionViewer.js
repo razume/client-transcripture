1dcc1ab06834437463f2d192a3f286474dcd5f47
tannerbyers <programtanner@gmail.com>
2020-04-23 23:03:30 -0400

    GOT VIDEO

--- src/components/TranscriptionViewer.js
+++ src/components/TranscriptionViewer.js
@@ -7,17 +7,11 @@ import axios from "axios"
 const TranscriptionViewer = ({ TranscriptionData, setFileSelect, posturl }) => {
     console.log(TranscriptionData)
 
-    useEffect(() => {
-        console.log("is this being called?")
-        axios
-            .get(`${posturl}/api/video`)
-            .then((res) => console.log("res received ", res.data));
-    })
-
     return (
         <ThemeProvider theme={theme}>
             <Box style={{ padding: "5rem" }} width="30rem" id="PutSampleVideo">
                 <button onClick={() => setFileSelect(false)}> Go back</button>
+                <video src={TranscriptionData.playUrl} controls></video>
                 <p style={{ whiteSpace: "pre-line" }}>{TranscriptionData.content}</p>
             </Box>
         </ThemeProvider>
