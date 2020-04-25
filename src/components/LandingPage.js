f6f1458eeedfe4362800fb47bfbed08291619a84
Leighton A Pitman <leightonpitman14@gmail.com>
2020-04-24 23:59:33 -0400

    style improvements to first part of landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -41,68 +41,93 @@ const LandingPage = ({ redirectURL, clientID, posturl }) => {
 
   return (
     <Box>
-      <NavBar>
-        <Box display="flex" flexDirection="row" alignItems="center">
-          <img
-            className="top-left-logo"
-            src={require("../media/scribe_circle_dark.svg")}
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="50rem"
+        border="3px solid red"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1340 320"
+          style={{ zIndex: -1, position: "absolute" }}
+        >
+          <path
+            fill="#a2d9ff"
+            fill-opacity="0.5"
+            d="M0,96L30,128C60,160,120,224,180,229.3C240,235,300,181,360,133.3C420,85,480,43,540,21.3C600,0,660,0,720,53.3C780,107,840,213,900,240C960,267,1020,213,1080,160C1140,107,1200,53,1260,80C1320,107,1380,213,1410,266.7L1440,320L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"
+          ></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="300 0 1040 320"
+          style={{ zIndex: -1, position: "absolute", marginTop: "20rem" }}
+        >
+          <path
+            fill="#a2d9ff"
+            fill-opacity="0.3"
+            d="M0,160L24,133.3C48,107,96,53,144,32C192,11,240,21,288,37.3C336,53,384,75,432,101.3C480,128,528,160,576,186.7C624,213,672,235,720,218.7C768,203,816,149,864,144C912,139,960,181,1008,197.3C1056,213,1104,203,1152,176C1200,149,1248,107,1296,117.3C1344,128,1392,192,1416,224L1440,256L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"
+          ></path>
+        </svg>
+        <NavBar>
+          <Box display="flex" flexDirection="row" alignItems="center">
+            <img
+              className="top-left-logo"
+              src={require("../media/scribe_logo_text.svg")}
+            />
+          </Box>
+          <Box>
+            <Button>About</Button>
+            <Button href={url}>Log in</Button>
+          </Box>
+        </NavBar>
+
+        <Box display="flex" justifyContent="center">
+          <Heading
+            textAlign="center"
+            fontWeight="500"
+            fontSize="24pt"
+            minWidth="25rem"
+            maxWidth="50rem"
+            color={theme.colors.gray[7]}
+          >
+            Get the most out of Zoom. View your team's recorded meetings,
+            transcripts of the meetings, and reports.
+          </Heading>
+        </Box>
+
+        <Box
+          display="flex"
+          justifyContent="space-around"
+          flexWrap="wrap"
+          mt={5}
+        >
+          <PostCard
+            imageUrl={require("../media/typewriter.jpg")}
+            cardTitle={"Transcripts"}
+            cardBody={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
+          />
+          <PostCard
+            imageUrl={require("../media/camcorder.jpg")}
+            cardTitle={"Recorded Meetings"}
+            cardBody={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
+          />
+
+          <PostCard
+            imageUrl={require("../media/calculator.jpg")}
+            cardTitle={"Reports"}
+            cardBody={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
           />
         </Box>
-        <Box>
-          <Button>About</Button>
-          <Button href={url}>Log in</Button>
-        </Box>
-      </NavBar>
-      <svg
-        className="background-wave"
-        viewBox="0 0 500 200"
-        preserveAspectRatio="xMinYMin meet"
-      >
-        <path
-          d="M0,200 C350,100 350,0 500,50 L500,00 L0,0 Z"
-          style={{ stroke: "none", fill: "#E4ECED" }}
-        />
-      </svg>
-
-      <Box display="flex" justifyContent="center">
-        <Heading
-          textAlign="center"
-          fontWeight="400"
-          minWidth="25rem"
-          maxWidth="50rem"
-          color={theme.colors.gray[6]}
-        >
-          Get the most out of Zoom. View your team's recorded meetings,
-          transcripts of the meetings, and reports.
-        </Heading>
       </Box>
 
-      <Box display="flex" justifyContent="space-around" flexWrap="wrap" mt={5}>
-        <PostCard
-          imageUrl={require("../media/typewriter.jpg")}
-          cardTitle={"Transcripts"}
-          cardBody={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          }
-        />
-        <PostCard
-          imageUrl={require("../media/camcorder.jpg")}
-          cardTitle={"Recorded Meetings"}
-          cardBody={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          }
-        />
-
-        <PostCard
-          imageUrl={require("../media/calculator.jpg")}
-          cardTitle={"Reports"}
-          cardBody={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          }
-        />
-      </Box>
       <Box
-        mt="12rem"
         height="55rem"
         width="100%"
         backgroundColor="#36404f"
